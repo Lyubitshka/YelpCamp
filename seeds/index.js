6c64a046f55a29e8ccb49315bdd2a47981d6fba0
Lyubitshka <antwarowska@gmail.com>
2022-10-25 21:05:42 +0200

    Basic Authorisation

--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async() => {
 	    const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
 	    const camp = new Campground({
+            author: '6357ec266c0f3c4e420562b3',
     		location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://random.imagecdn.app/500/150', 
